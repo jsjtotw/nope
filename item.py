cc3548ade7e330b661b2065efae9ed2d6fd594d7
UnnormalPerson <135814712+UnnormalPerson@users.noreply.github.com>
2023-06-26 10:14:25 +0800

    Update item.py

--- item.py
+++ item.py
@@ -1,2 +1,4 @@
 tandas = input('What is your name')
+jamban = input('Who is your girlfriend? (This will not be recorded.)')
 print(tandas 'is gay.')
+print(jamban 'is also gay and is as retarded as you are currently')
