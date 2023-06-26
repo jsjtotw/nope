2c57f1c232b76bf5d705d5a428ca6a71dae782b1
jsjtotw <134265805+jsjtotw@users.noreply.github.com>
2023-06-26 13:45:01 +0800

    Update item.py

--- item.py
+++ item.py
@@ -1,12 +1,22 @@
 tandas = input('What is your name')
-gen = input('What is your gender? (m/f)')
-if gen == 'm':
-    par = "girlfriend"
-elif gen == 'f':
-    par = "boyfriend"
-else:
+if tandas == 'joshua':
     print('Error')
-
-jamban = input(f'Who is your {par} (This will not be recorded.)')
-print(f'{tandas} is gay.')
-print(f'{jamban} is also gay and is as retarded as you are currently are!')
+elif tandas == 'Joshua':
+    print('Error')
+else:
+    gen = input('What is your gender? (m/f)')
+    if gen == 'm':
+        par = "girlfriend"
+    elif gen == 'f':
+        par = "boyfriend"
+    else:
+        print('Error')
+    
+    jamban = input(f'Who is your {par} (This will not be recorded.)')
+    if jamban == 'joshua':
+        print('Error')
+    elif jamban == 'Joshua':
+        print('Error')
+    else:
+        print(f'{tandas} is gay.')
+        print(f'{jamban} is also gay and is as retarded as you are currently are!')
