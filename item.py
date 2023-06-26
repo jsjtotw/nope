f55e8e28fa26946eb21ae86b8982d28bcd424bcb
jsjtotw <134265805+jsjtotw@users.noreply.github.com>
2023-06-26 13:15:27 +0800

    Update item.py

--- item.py
+++ item.py
@@ -1,4 +1,12 @@
 tandas = input('What is your name')
-jamban = input('Who is your girlfriend? (This will not be recorded.)')
+gen = input('What is your gender? (m/f)')
+if gen == 'm':
+    par = "girlfriend"
+elif gen == 'f':
+    par = "boyfriend"
+else:
+    print('Error')
+
+jamban = input(f'Who is your {par} (This will not be recorded.)')
 print(f'{tandas} is gay.')
-print(f'{tandas} is also gay and is as retarded as you are currently are!')
+print(f'{jamban} is also gay and is as retarded as you are currently are!')
