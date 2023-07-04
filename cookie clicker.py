99fc4176ac08fcfd372a90224f0416bde2496098
jsjtotw <134265805+jsjtotw@users.noreply.github.com>
2023-07-04 09:59:54 +0800

    Update cookie clicker.py

--- cookie clicker.py
+++ cookie clicker.py
@@ -1,22 +1,34 @@
-import turtle
-
-def clicked(x, y):
-    global clicks
-    clicks += 1
-    pen.clear()
-    pen.write(f"Clicks: {clicks}", align="center", font=("Courier New", 32, "normal"))
-wn = turtle.Screen()
-wn.bgcolor("black")
-wn.addshape("cookie.gif")
-cookie = turtle.Turtle()
-cookie.shape("cookie.gif")
-cookie.speed(0)
-clicks = 0
-pen = turtle.Turtle()
-pen.hideturtle()
-pen.color("white")
-pen.penup()
-pen.goto(0, 400)
-pen.write(f"Clicks: {clicks}", align="center", font=("Courier New", 32, "normal"))
-cookie.onclick(clicked)
-turtle.mainloop()
+import pygame
+import sys
+pygame.init()
+WIDTH = 800
+HEIGHT = 600
+WHITE = (255, 255, 255)
+cookie_pos = (WIDTH // 2, HEIGHT // 2)
+cookie_radius = 69
+cookie_color = (204, 204, 0)
+score = 0
+font_size = 36
+font_color = WHITE
+font_pos = (20, 20)
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                mouse_pos = pygame.mouse.get_pos()
+                distance = ((cookie_pos[0] - mouse_pos[0]) ** 2 +
+                            (cookie_pos[1] - mouse_pos[1]) ** 2) ** 0.5
+                if distance < cookie_radius:
+                    score += 1
+        window.fill((0, 0, 0))
+        pygame.draw.circle(window, cookie_color, cookie_pos, cookie_radius)
+        font = pygame.font.Font(None, font_size)
+    text = font.render(f"Score: {score}", True, font_color)
+    window.blit(text, font_pos)
+    pygame.display.flip()
+    clock.tick(60)
