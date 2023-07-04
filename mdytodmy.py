110cbafef997a342d499d278f8f037df30da1715
jsjtotw <134265805+jsjtotw@users.noreply.github.com>
2023-07-04 23:56:28 +0800

    Update mdytodmy.py

--- mdytodmy.py
+++ mdytodmy.py
@@ -1,11 +1,12 @@
 import re
 from datetime import datetime
 def change_date_format(file_path):
-    with open(file_path, 'r') as file:
+    with open(file_path, 'r', encoding='utf-8') as file:
         content = file.read()
     pattern = r'(\d{1,2})/(\d{1,2})/(\d{2,4})'
     new_content = re.sub(pattern, r'\2/\1/\3', content)
-    with open(file_path, 'w') as file:
+    with open(file_path, 'w', encoding='utf-8') as file:
         file.write(new_content)
-file_path = 'your_file_path.txt'
+file_path = "WhatsApp Chat with AHS but it's 1C.txt"
 change_date_format(file_path)
+print("Finished! Lucas x Alden")
